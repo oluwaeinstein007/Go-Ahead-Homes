3f3dc7faae1250fc80bfdac9fa17d14a18712592
Segun Akanbi <akanbisegun46@yahoo.com>
2021-07-28 08:45:37 +0100

    fix: popup modal and added image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,24 +14,26 @@
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <link href="/css/app/.css" rel="stylesheet">
         <!-- Scripts -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
 
-        
+
     </head>
     <body>
     <script>
          $(document).ready(function(){
-             $('#exampleModal').modal.('show');
-         })
-        </script>
+             $('#exampleModal').modal('show');
+         });
+    </script>
 
    <div class="welcome justify-content-center">
 
    <div class="front">
         <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+{{-- <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
   Launch demo modal
-</button>
+</button> --}}
 
 <!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -44,14 +46,14 @@
         </button>
       </div>
       <div class="modal-body front">
-        <p>This policy outlines how Go Ahead Homes will continue to execute the safe delivery of 
-our service during this COVID-19 pandemic and what actions will be taken in the event 
+        <p>This policy outlines how Go Ahead Homes will continue to execute the safe delivery of
+our service during this COVID-19 pandemic and what actions will be taken in the event
 of an outbreak within our service.
-To date, we have not had any confirmed cases of COVID-19 virus amongst service 
-users, staff or their families, and continue to operate our service with minimal 
-disruptions as possible in line with governmental guidance. The safety and well-being of 
+To date, we have not had any confirmed cases of COVID-19 virus amongst service
+users, staff or their families, and continue to operate our service with minimal
+disruptions as possible in line with governmental guidance. The safety and well-being of
 our service users and staff team is extremely paramount during these times and we are
-therefore striving above and beyond to try and keep everyone safe from the effects of 
+therefore striving above and beyond to try and keep everyone safe from the effects of
 COVID-19</p>
       </div>
       <div class="modal-footer">
@@ -64,20 +66,19 @@ COVID-19</p>
 
    </div>
 
-  
+   <img src="{{asset('images/Project-Prints.png')}}" class="img-fluid" alt="Responsive image">
    <h2>WELCOME TO GO AHEAD HOMES</h2>
 
         <a href="{{ url('/first') }}"><button>come in</button></a>
-       
+
    </div>
         <div class="overlay"></div>
-    
 
 
 
-      
-           
-        
-        
+
+
+
+
     </body>
 </html>
